a1b2532bbf1154ff7fb76d8dffe834b77be15ebb
helloshiki <aquariusye@163.com>
2015-08-27 11:13:05 +0800

    add luv

---
+++ luv/luv/tests/run.lua
@@ -0,0 +1,34 @@
+-- Run this from the parent directory as
+--
+--     luajit tests/run.lua
+--
+
+local tap = require("lib/tap")
+local uv = require("luv")
+
+local isWindows
+if jit and jit.os then
+  -- Luajit provides explicit platform detection
+  isWindows = jit.os == "Windows"
+else
+  -- Normal lua will only have \ for path separator on windows.
+  isWindows = package.config:find("\\") and true or false
+end
+_G.isWindows = isWindows
+
+local req = uv.fs_scandir("tests")
+
+repeat
+  local ent = uv.fs_scandir_next(req)
+
+  if not ent then
+    -- run the tests!
+    tap(true)
+  end
+  local match = string.match(ent.name, "^test%-(.*).lua$")
+  if match then
+    local path = "tests/test-" .. match
+    tap(match)
+    require(path)
+  end
+until not ent
