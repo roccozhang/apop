e881be1f5d625cf7384c6427b6f02d5a3b3d8443
helloshiki <aquariusye@163.com>
2015-09-28 18:43:46 +0800

    default search path

--- lua53/lua-5.3.1/src/luaconf.h
+++ lua53/lua-5.3.1/src/luaconf.h
@@ -192,11 +192,11 @@
 #define LUA_LDIR	LUA_ROOT "share/lua/" LUA_VDIR "/"
 #define LUA_CDIR	LUA_ROOT "lib/lua/" LUA_VDIR "/"
 #define LUA_PATH_DEFAULT  \
-		LUA_LDIR"?.lua;"  LUA_LDIR"?/init.lua;" \
+		"/usr/lib/lua/?.lua;" LUA_LDIR"?.lua;"  LUA_LDIR"?/init.lua;" \
 		LUA_CDIR"?.lua;"  LUA_CDIR"?/init.lua;" \
 		"./?.lua;" "./?/init.lua"
 #define LUA_CPATH_DEFAULT \
-		LUA_CDIR"?.so;" LUA_CDIR"loadall.so;" "./?.so"
+		"/usr/lib/lua/?.so;" LUA_CDIR"?.so;" LUA_CDIR"loadall.so;" "./?.so"
 #endif			/* } */
 
 
