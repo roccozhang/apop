cb033865c725f7d3c15fdfcba7984f63d59d2e31
helloshiki <aquariusye@163.com>
2015-09-29 14:05:01 +0800

    stop all lua process

--- myinit/root/ugw/script/stop_all.sh
+++ myinit/root/ugw/script/stop_all.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-/etc/init.d/essential stop
-killstr "essential/main.lua"
+/ugw/script/cfgmgr stop
+/ugw/script/essential stop 
+/ugw/script/logserver stop
+/ugw/script/rds stop
+/ugw/script/sands stop
+/ugw/script/status stop
+/ugw/script/tenacious stop
+/ugw/script/userauth stop
+/ugw/script/websrv stop
