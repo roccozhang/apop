22ee6c6a1f51f77bc812c8ad31237b0cb9cc6522
helloshiki <aquariusye@163.com>
2015-10-14 17:17:45 +0800

    ap rom

--- myinit/root/ugw/script/checkaprom.lua
+++ myinit/root/ugw/script/checkaprom.lua
@@ -27,26 +27,18 @@ local function get_latest_version(host, rtype)
 	return res
 end
 
-local cmdmap = {}
-
 local function check_host_type(host, rtype)
 	assert(host and rtype)
 	local host = host == "default" and "cloud.i-wiwi.com:8081" or host
 	return host, rtype
 end
 
-function cmdmap.version(host, rtype)
+function get_version(host, rtype)
 	local host, rtype = check_host_type(host, rtype)
-	local version = get_latest_version(host, rtype)
-	local fmt = '{"st":%d,"data":"%s"}'
-	if version then 
-		io.stdout:write(string.format(fmt, 0, version))
-		os.exit(0)
-	end
-	io.stdout:write(string.format(fmt, 1, "get version fail"))
+	return get_latest_version(host, rtype)
 end
 
-function cmdmap.download(host, rtype)
+function download(host, rtype)
 	local host, rtype = check_host_type(host, rtype)
 	local download_dir = "/tmp/tmp_aprom"
 	local cmd = string.format([[
@@ -55,30 +47,69 @@ function cmdmap.download(host, rtype)
 			wget -O %s/imagex 'http://%s/m50?cmd=getlastestfirmwarefile&type=%s' 2>&1
 		]], download_dir, download_dir, download_dir, host, rtype)
 	local fmt = '{"st":%d,"data":"%s"}'
+	print(cmd)
 	local ret = os.execute(cmd)
 	if ret ~= 0 then 
-		io.stdout:write(string.format(fmt, 1, "download fail"))
+		print(string.format(fmt, 1, "download fail"))
 		os.execute("rm -rf " .. download_dir)
-		os.exit(1)
+		return false 
 	end
 	
 	local tmp_release = string.format("%s/tmp_release", download_dir)
 	os.execute("rm -rf " .. tmp_release) 
 	local cmd = string.format("/ugw/script/openssltar.sh untar %s/imagex %s wjrc0409", download_dir, tmp_release)
+	print(cmd)
 	local ret = os.execute(cmd)
 	if ret ~= 0 then 
-		io.stdout:write(string.format(fmt, 1, "download fail 2"))
+		print(string.format(fmt, 1, "download fail 2"))
 		os.execute("rm -rf " .. download_dir)
-		os.exit(1) 
+		return false
 	end 
 
 	local cmd = string.format("rm /tmp/www/webui/rom/%s*; mv %s/* /tmp/www/webui/rom/", rtype, tmp_release)
 	os.execute(cmd)
+	print(cmd)
 	os.execute("rm -rf " .. download_dir)
-	io.stdout:write(string.format(fmt, 0, "download ok"))
+	print(string.format(fmt, 0, "download ok"))
+	return true
+end
+
+local cmdmap = {}
+function cmdmap.version(args)
+	local host = table.remove(args, 1)
+
+	local version_map = {}
+	for k, v in ipairs(args) do 
+		local version = get_version(host, v)
+		if version then 
+			version_map[v] = version
+		end
+	end 
+
+	local arr = {}
+	for k, v in pairs(version_map) do 
+		table.insert(arr, string.format("%s:%s", k, v))
+	end 
+
+	local fp = io.open("/tmp/ap.version", "w")
+	fp:write(table.concat(arr, "\n"))
+	fp:close()
+end
+
+function cmdmap.download(args)
+	local host = table.remove(args, 1)
+	local lfs = require("lfs")
+	local version_map = {}
+	for k, v in ipairs(args) do 
+		local version = get_version(host, v)
+		if version then 
+			local path = string.format("/tmp/www/webui/rom/%s", version:gsub("%-", "."))
+			local _ = lfs.attributes(path) or download(host, v) 
+		end
+	end
 end
 
 local args = {...}
 local cmd = table.remove(args, 1)
 local func = cmdmap[cmd]
-local _ = func and func(unpack(args))
+local _ = func and func(args)
--- rds/root/ugw/apps/rds/apmfirewarelist.lua
+++ rds/root/ugw/apps/rds/apmfirewarelist.lua
@@ -21,19 +21,71 @@ local function apmupdatefireware(conn, group, data)
 end
 
 local function apmfirewarelist(conn, group, data) 
-	if not lfs.attributes("/www/rom") then 
+	local romdir = "/tmp/www/webui/rom"
+	if not lfs.attributes(romdir) then 
 		return {status = 0, data = {}}	
 	end 
 
 	local vers = {}
-	for filename in lfs.dir("/www/rom/") do 
+	for filename in lfs.dir(romdir) do 
 		local version = filename:match("(.+%.%d%d%d%d%d%d%d%d%d%d%d%d)")
 		local _ = version and table.insert(vers, version)
 	end
 	return {status = 0, data = vers}	
 end
 
+local function newest_version(conn, group, data) 
+	local data = {host = "default", arr = {"QM1439", "QM1438"}}
+	local map = data 
+	local host, arr = map.host, map.arr 
+	if not (host and arr and #arr > 0) then 
+		return {status = 1, data = "invalid param"}	 
+	end 
+
+	local narr = {"version", host}
+	for _, v in ipairs(arr) do 
+		table.insert(narr, v)
+	end 
+
+	local cmd = string.format("timeout -t 4 lua /ugw/script/checkaprom.lua %s", table.concat(narr, " "))
+	print("TODO cmd", cmd)
+	-- os.execute(cmd)
+	local fp = io.open("/tmp/ap.version", "r")
+	if not fp then 
+		return {status = 1, "check version fail"}
+	end 
+
+	local narr = {}
+	for line in fp:lines() do 
+		table.insert(narr, line)
+	end 
+	fp:close()
+
+	return {status = 0, data = line}
+end
+
+local function download_rom(conn, group, data) 
+	local data = {host = "default", arr = {"QM1439", "QM1438"}}
+	local map = data 
+	local host, arr = map.host, map.arr 
+	if not (host and arr and #arr > 0) then 
+		return {status = 1, data = "invalid param"}	 
+	end 
+
+	local narr = {"download", host}
+	for _, v in ipairs(arr) do 
+		table.insert(narr, v)
+	end 
+
+	local cmd = string.format("lua /ugw/script/checkaprom.lua %s &", table.concat(narr, " "))
+	print("TODO cmd", cmd)
+	-- os.execute(cmd)
+	return {status = 0, data = "ok"}
+end
+
 return {
+	download_rom = download_rom,
+	newest_version = newest_version,
 	apmfirewarelist = apmfirewarelist,
 	apmupdatefireware = apmupdatefireware,
 }
